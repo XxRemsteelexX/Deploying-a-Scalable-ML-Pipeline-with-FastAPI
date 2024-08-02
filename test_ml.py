ad1886386842b95e0596098c1e34ce8aeb63cfbc
Glenn Dalbey <dalbeyglenn@gmail.com>
2024-08-02 00:10:57 -0500

    updating recent files

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,70 @@
+# Databricks notebook source
+
 import pytest
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from ml.model import train_model
+from sklearn.ensemble import RandomForestClassifier
+from pathlib import Path
+
+
+
 # TODO: add necessary import
 
+
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_test_split_size():
     """
-    # add description for the first test
+    checking that the sliced data is ready for testing
     """
     # Your code here
-    pass
+    data_path = './data/census.csv'
+    data = pd.read_csv(str(data_path))
+    train, test = train_test_split(data, test_size = 0.2)
+    assert len(test) >= 2000
+    
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_column_names():
     """
-    # add description for the second test
+    testing that all features are in the data
     """
     # Your code here
-    pass
+    data_path = './data/census.csv'
+    data = pd.read_csv(data_path)
+
+    features = {
+        'age',
+        'workclass',
+        'fnlgt',
+        'education',
+        'education-num',
+        'marital-status',
+        'occupation',
+        'relationship',
+        'race',
+        'sex',
+        'capital-gain',
+        'capital-loss',
+        'hours-per-week',
+        'native-country',
+        'salary'
+    }
+
+    assert set(data.columns) == features
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_model_type():
     """
-    # add description for the third test
+    testing that the model is random forest classifier 
     """
     # Your code here
-    pass
+    sample_x = [[0, 1, 2], [3, 4, 5]]
+    sample_y = ['col1', 'col2']
+
+    model = train_model(sample_x, sample_y)
+
+    assert isinstance(model, RandomForestClassifier)
